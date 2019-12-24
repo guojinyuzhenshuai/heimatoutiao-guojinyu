1b781052c63a0b14df99e4fb85e77c96d83e7fef
guojinyu <guojinyu@outlook.com>
2019-12-24 21:38:56 +0800

    黑马头条PC-内容列表-页面结构-搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -8,12 +8,12 @@
         <span>文章状态</span>
       </el-col>
       <el-col :span="18">
-        <el-radio-group v-model="radio">
-          <el-radio :label="1">全部</el-radio>
-          <el-radio :label="2">草稿</el-radio>
-          <el-radio :label="3">待审核</el-radio>
-          <el-radio :label="4">审核通过</el-radio>
-          <el-radio :label="5">审核失败</el-radio>
+        <el-radio-group v-model="formData.status">
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="1">草稿</el-radio>
+          <el-radio :label="2">待审核</el-radio>
+          <el-radio :label="3">审核通过</el-radio>
+          <el-radio :label="4">审核失败</el-radio>
         </el-radio-group>
       </el-col>
     </el-row>
@@ -22,22 +22,24 @@
         <span>频道列表</span>
       </el-col>
       <el-col :span="18">
-        <el-select></el-select>
+        <el-select v-model="formData.channel_id">
+          <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        </el-select>
       </el-col>
     </el-row>
     <el-row class="hgt">
-      <el-col span="4">
+      <el-col :span="4">
         <span>时间选择</span>
       </el-col>
-      <el-col span="18">
-        <el-time-picker
-          is-range
-          v-model="value1"
-          range-separator="至"
+      <el-col :span="18">
+        <el-date-picker
+          v-model="formData.dateRange"
+          type="daterange"
           start-placeholder="开始时间"
           end-placeholder="结束时间"
-          placeholder="选择时间范围"
-        ></el-time-picker>
+          range-separator="-"
+        ></el-date-picker>
+        {{formData.dateRange}}
       </el-col>
     </el-row>
   </el-card>
@@ -51,8 +53,28 @@ export default {
   },
   data () {
     return {
-      radio: {}
+      formData: {
+        status: 5,
+        dateRange: [],
+        value: '', // 定义一个空数组接收
+        channel_id: null
+      },
+      channels: []
     }
+  },
+  methods: {
+    getArticles () {
+      this.$axios({
+        url: '/channels',
+        method: 'get'
+      }).then(result => {
+        this.channels = result.data.channels
+        console.log(this.channels)
+      })
+    }
+  },
+  created () {
+    this.getArticles()
   }
 }
 </script>
