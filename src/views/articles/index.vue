2d93de453e6903feb12613a5e05c24db03373502
guojinyu <guojinyu@outlook.com>
2019-12-24 22:00:39 +0800

    黑马头条PC-内容列表-页面主体结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -39,8 +39,32 @@
           end-placeholder="结束时间"
           range-separator="-"
         ></el-date-picker>
-        {{formData.dateRange}}
       </el-col>
+    </el-row>
+    <!-- 主体 -->
+    <el-row class='total'>
+        <span>共找到1000条符合条件的内容</span>
+    </el-row>
+    <!-- 循环的模板 -->
+    <el-row  v-for="item in 100" :key="item" class='article-item' type='flex' justify="space-between">
+        <!-- 左侧 -->
+       <el-col :span="14">
+           <el-row type='flex'>
+             <img src="../../assets/404.png" alt="">
+              <div class='info'>
+                <span>年少不听李宗盛，听懂己是不惑年。</span>
+                <el-tag class='tag'>标签一</el-tag>
+                <span class='date'>2019-12-24 09:15:42</span>
+              </div>
+           </el-row>
+       </el-col>
+       <!-- 右侧 -->
+       <el-col :span="6">
+           <el-row class='right' type='flex' justify="end">
+               <span><i class="el-icon-edit"></i>修改</span>
+               <span><i class="el-icon-delete"></i> 删除</span>
+           </el-row>
+       </el-col>
     </el-row>
   </el-card>
 </template>
@@ -83,5 +107,29 @@ export default {
 .hgt {
   height: 60px;
   padding-left: 100px;
+}
+.total{
+    margin:60px 0;
+}
+.article-item{
+    img{
+        width: 100px;
+        height: 100px;
+        margin-right:10px;
+        border-radius: 4px
+    }
+.info{
+    height: 100px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .tag{
+        max-width: 60px;
+    }
+    .date{
+        color: #999;
+        font-size:12px;
+    }
+}
 }
 </style>
