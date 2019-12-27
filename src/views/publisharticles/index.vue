326429c1e0e28c497aa5d8336d0c38a8881a5d59
guojinyu <guojinyu@outlook.com>
2019-12-27 16:18:37 +0800

    黑马头条PC-发表文章-发布文章-简单发布文章

--- src/views/publisharticles/index.vue
+++ src/views/publisharticles/index.vue
@@ -12,10 +12,10 @@
       </el-form-item>
       <el-form-item prop="type" label="封面" class="publishstyle" >
           <el-radio-group v-model="formData.cover.type">
-            <el-radio label="1">单图</el-radio>
-            <el-radio label="3">三图</el-radio>
-            <el-radio label="0">无图</el-radio>
-            <el-radio label="-1">自动</el-radio>
+            <el-radio :label="1">单图</el-radio>
+            <el-radio :label="3">三图</el-radio>
+            <el-radio :label="0">无图</el-radio>
+            <el-radio :label="-1">自动</el-radio>
           </el-radio-group>
       </el-form-item>
       <el-form-item prop="channel_id" label="频道"  class="publishstyle">
@@ -27,8 +27,8 @@
             </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="publishArticles">发布</el-button>
-        <el-button>存入草稿</el-button>
+        <el-button type="primary" @click="publishArticles(false)">发布</el-button>
+        <el-button @click="publishArticles(true)">存入草稿</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -47,7 +47,7 @@ export default {
         title: '', // 标题
         content: '', // 文章内容
         cover: {
-          type: '0', // 封面类型 1:自动 0-无图 1-1张 3-3张
+          type: 0, // 封面类型 1:自动 0-无图 1-1张 3-3张
           images: []// 存储的图片地址
         },
         channel_id: null // 频道id
@@ -63,10 +63,21 @@ export default {
     }
   },
   methods: {
-    publishArticles () {
-      this.$refs.publishForm.validate(function (isOk) {
+    publishArticles (draft) {
+      console.log(this.formData)
+
+      this.$refs.publishForm.validate(isOk => {
         if (isOk) {
-          console.log('OK')
+          this.$axios({
+            url: '/articles',
+            method: 'post',
+            params: {
+              draft
+            },
+            data: this.formData
+          }).then(() => {
+            this.$router.push('/home/articles')
+          })
         }
       })
     },
