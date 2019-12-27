527ea68e0af35325a41c3972aae8542526891620
guojinyu <guojinyu@outlook.com>
2019-12-27 15:07:55 +0800

    黑马头条PC-发表文章-频道数据加载

--- src/views/publisharticles/index.vue
+++ src/views/publisharticles/index.vue
@@ -3,6 +3,46 @@
     <bread-crumb slot="header">
       <template slot="title">发表文章</template>
     </bread-crumb>
+    <el-row style="margin-left:50px" label-width="100px">
+      <el-row class="publishstyle">
+        <el-col :span="1">标题</el-col>
+        <el-col :span="8">
+          <el-input placeholder="文章名称"></el-input>
+        </el-col>
+      </el-row>
+      <el-row class="publishstyle">
+        <el-col :span="1">内容</el-col>
+        <el-col :span="12">
+          <el-input type="textarea" :rows="10"></el-input>
+        </el-col>
+      </el-row>
+      <el-row class="publishstyle">
+        <el-col span="1">封面</el-col>
+        <el-col span="12">
+          <el-radio-group v-model="radio">
+            <el-radio>单图</el-radio>
+            <el-radio>三图</el-radio>
+            <el-radio>无图</el-radio>
+            <el-radio>自动</el-radio>
+          </el-radio-group>
+        </el-col>
+      </el-row>
+      <el-row class="publishstyle">
+        <el-col span="1">频道</el-col>
+        <el-col span="12">
+            <el-select>
+              <el-option v-for="item in channels"
+            :key="item.id.toString()"
+            :label="item.name"
+            :value="item.id"></el-option>
+            </el-select>
+        </el-col>
+      </el-row>
+      <el-row>
+          <el-col></el-col>
+          <el-col></el-col>
+      </el-row>
+    </el-row>
   </el-card>
 </template>
 
@@ -11,9 +51,34 @@ import BreadCrumb from '../../common/bread-crumb'
 export default {
   components: {
     'bread-crumb': BreadCrumb
+  },
+  data () {
+    return {
+      channels: []
+
+    }
+  },
+  methods: {
+    getArticles () {
+      this.$axios({
+        url: '/channels',
+        method: 'get'
+      }).then(result => {
+        this.channels = result.data.channels
+        console.log(this.channels)
+      })
+    }
+  },
+  created () {
+    this.getArticles()
   }
 }
 </script>
 
-<style>
+<style lang="less" spoced>
+.publishstyle {
+  // height: 130px;
+  margin-left: 100px;
+  margin-top: 60px
+}
 </style>
