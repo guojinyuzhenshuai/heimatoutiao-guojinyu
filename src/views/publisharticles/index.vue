131ca423d94c77254f60f69f5f04f9af5042f9c3
guojinyu <guojinyu@outlook.com>
2019-12-27 19:12:26 +0800

    黑马头条PC-发布文章-封面功能设计

--- src/views/publisharticles/index.vue
+++ src/views/publisharticles/index.vue
@@ -3,28 +3,43 @@
     <bread-crumb slot="header">
       <template slot="title">发表文章</template>
     </bread-crumb>
-    <el-form ref="publishForm" :model="formData" :rules="publishRules" style="margin-left:50px" label-width="100px">
+    <el-form
+      ref="publishForm"
+      :model="formData"
+      :rules="publishRules"
+      style="margin-left:50px"
+      label-width="100px"
+    >
       <el-form-item prop="title" label="标题" class="publishstyle">
-          <el-input v-model="formData.title" placeholder="文章名称"></el-input>
+        <el-input v-model="formData.title" placeholder="文章名称"></el-input>
       </el-form-item>
       <el-form-item prop="content" label="内容" class="publishstyle">
-          <quill-editor style="height:400px" v-model="formData.content"  type="textarea" :formel-forms="10"></quill-editor>
+        <quill-editor
+          style="height:400px"
+          v-model="formData.content"
+          type="textarea"
+          :formel-forms="10"
+        ></quill-editor>
       </el-form-item>
       <el-form-item prop="type" label="封面" class="publishstyle" style="margin-top:100px">
-          <el-radio-group v-model="formData.cover.type">
-            <el-radio :label="1">单图</el-radio>
-            <el-radio :label="3">三图</el-radio>
-            <el-radio :label="0">无图</el-radio>
-            <el-radio :label="-1">自动</el-radio>
-          </el-radio-group>
+        <el-radio-group  v-model="formData.cover.type">
+          <!-- @change="changeType" -->
+          <el-radio :label="1">单图</el-radio>
+          <el-radio :label="3">三图</el-radio>
+          <el-radio :label="0">无图</el-radio>
+          <el-radio :label="-1">自动</el-radio>
+        </el-radio-group>
+        {{formData.cover}}
       </el-form-item>
-      <el-form-item prop="channel_id" label="频道"  class="publishstyle">
-            <el-select v-model="formData.channel_id">
-              <el-option v-for="item in channels"
+      <el-form-item prop="channel_id" label="频道" class="publishstyle">
+        <el-select v-model="formData.channel_id">
+          <el-option
+            v-for="item in channels"
             :key="item.id.toString()"
             :label="item.name"
-            :value="item.id"></el-option>
-            </el-select>
+            :value="item.id"
+          ></el-option>
+        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="publishArticles(false)">发布</el-button>
@@ -48,15 +63,20 @@ export default {
         content: '', // 文章内容
         cover: {
           type: 0, // 封面类型 1:自动 0-无图 1-1张 3-3张
-          images: []// 存储的图片地址
+          images: [] // 存储的图片地址
         },
         channel_id: null // 频道id
       },
       publishRules: {
         // 表单校检规则
-        title: [{ required: true, message: '标题内容不能为空' }, {
-          min: 5, max: 30, message: '标题长度需要在5到30字符之间'
-        }],
+        title: [
+          { required: true, message: '标题内容不能为空' },
+          {
+            min: 5,
+            max: 30,
+            message: '标题长度需要在5到30字符之间'
+          }
+        ],
         content: [{ required: true, message: '文章内容不能为空' }],
         channel_id: [{ required: true, message: '文章频道不能为空' }]
       }
@@ -66,25 +86,42 @@ export default {
     $route: function (to, from) {
       if (Object.keys(to.params).length) {
         // 有参数修改
+        this.getArticlesById(to.params.articleId)
       } else {
         // 没有参数 => 发布
-        this.formData =
-        this.formData = {
+        this.formData = this.formData = {
           title: '', // 标题
           content: '', // 文章内容
           cover: {
             type: 0, // 封面类型 1:自动 0-无图 1-1张 3-3张
-            images: []// 存储的图片地址
+            images: [] // 存储的图片地址
           }
         }
+      }
+    },
+    // 监控嵌套对象的中值
+    'formData.cover.type': function () {
+    // this指向 组件实例
+      if (this.formData.cover.type === 0 || this.formData.cover.type === -1) {
+        // 无图模式
+        this.formData.cover.images = []
+      } else if (this.formData.cover.type === 1) {
+        // 单图模式
+        this.formData.cover.images = ['']
+      } else if (this.formData.cover.type === 3) {
+        // 三图模式
+        this.formData.cover.images = ['', '', '']
       }
     }
   },
   methods: {
+    // changeType () {
+    //   alert(this.formData.cover.type)
+    // },
     getArticlesById (articleId) {
       this.$axios({
         url: `articles/${articleId}`
-      }).then((result) => {
+      }).then(result => {
         this.formData = result.data
         console.log(this.formData)
       })
@@ -143,6 +180,6 @@ export default {
 .publishstyle {
   // height: 130px;
   margin-left: 100px;
-  margin-top: 60px
+  margin-top: 60px;
 }
 </style>
